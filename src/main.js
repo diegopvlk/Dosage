18678acbc59da96c09094822f096c1153e277206
Diego Povliuk <diego.pvlk@gmail.com>
2024-02-11 10:35:29 -0300

    build(release): bump version to 1.5.5

--- src/main.js
+++ src/main.js
@@ -42,7 +42,7 @@ export const DosageApplication = GObject.registerClass(
 					application_icon: 'io.github.diegopvlk.Dosage',
 					developer_name: 'Diego Povliuk',
 					developers: ['Diego Povliuk'],
-					version: '1.5.4',
+					version: '1.5.5',
 					issue_url: 'https://github.com/diegopvlk/Dosage/issues',
 					license_type: Gtk.License.GPL_3_0_ONLY,
 					copyright: '© 2023 Diego Povliuk',
